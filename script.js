057ae9da435805a1ad5722e10befd64fff16cb27
cabbagestamps <cabbagestamps@gmail.com>
2022-03-17 17:29:49 +1100

    Remove row classes, add cell class to all cells

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 
 let content = document.getElementById('content')
 
-for (let i=0; i<900; i++) {
+for (let i=0; i<500; i++) {
     let newDiv = document.createElement('div');
-    newDiv.id = `div${i}` 
+    newDiv.id = `div${i}`; 
+    newDiv.classList.add('cell');
     content.appendChild(newDiv);
-    newDiv.textContent = `div ${i}`
-    newDiv.addEventListener('mouseenter', () => {
-        newDiv.classList.add('active')
-    })
-    if (i<4) {
-        newDiv.classList.add('row1');   
-    } else if (i<8) {
-        newDiv.classList.add('row2');
-    } else if (i<12) {
-        newDiv.classList.add('row3');
-    } else {
-        newDiv.classList.add('row4');
-    }
+   // newDiv.textContent = `div ${i}`
+    newDiv.addEventListener('mousedown', () => {
+        newDiv.classList.add('active')   
+    }) 
 }
 
 
 
+
+
+/* Add a button to the top of the screen which will 
+clear the current grid and send the user a popup asking
+ for the number of squares per side for the new grid.
+  Once entered, the new grid should be generated in the same total
+   space as before (e.g. 960px wide) so that you’ve got 
+   a new sketch pad.
+*/
 
 
